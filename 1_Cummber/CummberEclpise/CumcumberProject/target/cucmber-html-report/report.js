6e56485517948190e0118505715701503d26c981
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-05 18:42:18 -0600

    no message

--- 1_Cummber/CummberEclpise/CumcumberProject/target/cucmber-html-report/report.js
+++ 1_Cummber/CummberEclpise/CumcumberProject/target/cucmber-html-report/report.js
@@ -1,191 +1,123 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("shopping.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Signup.feature");
 formatter.feature({
-  "line": 3,
-  "name": "Adding items to basket",
+  "line": 1,
+  "name": "Signup for account",
   "description": "",
-  "id": "adding-items-to-basket",
-  "keyword": "Feature",
+  "id": "signup-for-account",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 1024001,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 4696069,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Signup",
+  "description": "",
+  "id": "signup-for-account;signup",
+  "type": "scenario",
+  "keyword": "Scenario",
   "tags": [
     {
-      "line": 1,
-      "name": "@Smoke"
-    },
-    {
-      "line": 1,
-      "name": "@Sanity"
+      "line": 3,
+      "name": "@Signup"
     }
   ]
 });
-formatter.scenario({
+formatter.step({
   "line": 5,
-  "name": "No item selected in basket",
+  "name": "I want to signup into account",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "SignupStep.I_want_to_signup_into_account()"
+});
+formatter.result({
+  "duration": 1198400203,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 616448,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 31013920,
+  "status": "passed"
+});
+formatter.uri("login.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Login into account",
   "description": "",
-  "id": "adding-items-to-basket;no-item-selected-in-basket",
+  "id": "login-into-account",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 303104,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Valid username and password",
+  "description": "",
+  "id": "login-into-account;valid-username-and-password",
   "type": "scenario",
   "keyword": "Scenario"
+});
+formatter.step({
+  "line": 4,
+  "name": "I launch the application",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 5,
+  "name": "I enter correct username and password",
+  "keyword": "Then "
 });
 formatter.step({
   "line": 6,
-  "name": "I start the application",
-  "keyword": "Given "
+  "name": "Clicked on Login Button",
+  "keyword": "And "
 });
 formatter.step({
   "line": 7,
-  "name": "I should see a selection of products",
+  "name": "I am login into application",
   "keyword": "Then "
 });
-formatter.step({
-  "line": 8,
-  "name": "I browse \"without\" buying product",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 9,
-  "name": "my basket \"should\" be empty",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 10,
-  "name": "I \"cannot\" buy an item",
-  "keyword": "And "
-});
 formatter.match({
-  "location": "shoppingStep.I_start_the_application()"
+  "location": "LoginStep.I_launch_the_application()"
 });
 formatter.result({
-  "duration": 1508655355,
+  "duration": 1572866,
   "status": "passed"
 });
 formatter.match({
-  "location": "shoppingStep.I_should_see_a_selection_of_products()"
+  "location": "LoginStep.I_enter_correct_username_and_password()"
 });
 formatter.result({
-  "duration": 204799,
+  "duration": 675328,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "without",
-      "offset": 10
-    }
-  ],
-  "location": "shoppingStep.I_browse_without_buying_product(String)"
+  "location": "LoginStep.Clicked_on_Login_Button()"
 });
 formatter.result({
-  "duration": 30758385,
+  "duration": 162304,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "should",
-      "offset": 11
-    }
-  ],
-  "location": "shoppingStep.my_basket_should_be_empty(String)"
+  "location": "LoginStep.I_am_login_into_application()"
 });
 formatter.result({
-  "duration": 734720,
+  "duration": 641025,
   "status": "passed"
 });
-formatter.match({
-  "arguments": [
-    {
-      "val": "cannot",
-      "offset": 3
-    }
-  ],
-  "location": "shoppingStep.I_cannot_buy_an_item(String)"
-});
-formatter.result({
-  "duration": 1007616,
-  "status": "passed"
-});
-formatter.scenario({
-  "line": 12,
-  "name": "No item selected in basket",
-  "description": "",
-  "id": "adding-items-to-basket;no-item-selected-in-basket",
-  "type": "scenario",
-  "keyword": "Scenario"
-});
-formatter.step({
-  "line": 13,
-  "name": "I start the application",
-  "keyword": "Given "
-});
-formatter.step({
-  "line": 14,
-  "name": "I should see a selection of products",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 15,
-  "name": "I browse \"with\" buying product",
-  "keyword": "When "
-});
-formatter.step({
-  "line": 16,
-  "name": "my basket \"shouldnot\" be empty",
-  "keyword": "Then "
-});
-formatter.step({
-  "line": 17,
-  "name": "I \"can\" buy an item",
-  "keyword": "And "
-});
-formatter.match({
-  "location": "shoppingStep.I_start_the_application()"
-});
-formatter.result({
-  "duration": 37814764,
-  "status": "passed"
-});
-formatter.match({
-  "location": "shoppingStep.I_should_see_a_selection_of_products()"
-});
-formatter.result({
-  "duration": 1828351,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "with",
-      "offset": 10
-    }
-  ],
-  "location": "shoppingStep.I_browse_without_buying_product(String)"
-});
-formatter.result({
-  "duration": 618495,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "shouldnot",
-      "offset": 11
-    }
-  ],
-  "location": "shoppingStep.my_basket_should_be_empty(String)"
-});
-formatter.result({
-  "duration": 792064,
-  "status": "passed"
-});
-formatter.match({
-  "arguments": [
-    {
-      "val": "can",
-      "offset": 3
-    }
-  ],
-  "location": "shoppingStep.I_cannot_buy_an_item(String)"
-});
-formatter.result({
-  "duration": 1567232,
+formatter.after({
+  "duration": 444416,
   "status": "passed"
 });
 });
