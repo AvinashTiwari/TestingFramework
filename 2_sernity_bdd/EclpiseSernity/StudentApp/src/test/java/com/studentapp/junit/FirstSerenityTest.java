8ef7ac8e460b120fd8c2abc0da78c5e81d05c9e0
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-11 19:48:18 -0600

    no message

--- 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/FirstSerenityTest.java
+++ 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/FirstSerenityTest.java
@@ -1,13 +1,21 @@
 package com.studentapp.junit;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+import org.apache.xerces.util.SynchronizedSymbolTable;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import io.restassured.RestAssured;
 import net.serenitybdd.junit.runners.SerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
+import net.thucydides.core.annotations.Manual;
+import net.thucydides.core.annotations.Pending;
 
 @RunWith(SerenityRunner.class)
 public class FirstSerenityTest {
@@ -36,7 +44,37 @@ public class FirstSerenityTest {
 		.then()
 		.log()
 		.all()
-		.statusCode(200);
+		.statusCode(500);
+	}
+	
+	@Test
+	@Pending
+	public void thisPendingTest(){
+	
+	}
+	
+	@Test
+	@Ignore
+	public void ThisIsASkippedTest(){
+	
+	}
+	
+	@Test
+	public void ThisTestCauseError(){
+		System.out.println("Division Error " +  5/0);
+	
+	}
+	
+	@Test
+	public void filedoesnotExist() throws FileNotFoundException{
+      File newFile = new File("F://ss.as");
+      FileReader freader = new FileReader(newFile);
+	}
+	
+	@Test
+	@Manual
+	public void IsThisManulTest() throws FileNotFoundException{
+   
 	}
 
 }
