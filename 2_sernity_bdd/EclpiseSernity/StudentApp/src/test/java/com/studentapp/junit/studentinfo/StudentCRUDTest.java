ff1bcee1516b0caae4f27b09264c3919f93e1875
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-11 23:02:18 -0600

    no message

--- 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/studentinfo/StudentCRUDTest.java
+++ 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/studentinfo/StudentCRUDTest.java
@@ -29,7 +29,7 @@ public class StudentCRUDTest extends TestBase {
 	static String lastName =TestUtil.getRandomValue()+ "  -Tiwari";
 	static String programme = "ComputerScience";
 	static String email =  TestUtil.getRandomValue()+"abc@12376778.com";
-	
+	static int studentId ;
 	@Title("This test Will Create New Student")
 	@Test
 	public void test001(){
@@ -75,8 +75,75 @@ public class StudentCRUDTest extends TestBase {
 		.path("findAll{it.firstName=='"+firstName+"'}.get(0)");
 	
 	System.out.println("The value is " + value);
+	studentId = (int)value.get("id");
 	
 	assertThat(value, hasValue(firstName));
 	}
 
+	@Title("Update the User Information and Verify updated information")
+	@Test
+	public void test003(){
+		
+		ArrayList<String> course = new ArrayList<String>();
+		course.add("Java");
+		course.add("C++");
+		firstName = firstName + "_updated";
+		
+		StudentClass student = new StudentClass();
+		student.setFirstName(firstName);
+		student.setLastName(lastName);
+		student.setProgramme(programme);
+		student.setEmail(email);
+		student.setCourses(course);
+		
+		SerenityRest.rest().given()
+		.contentType(ContentType.JSON)
+		.log()
+		.all()
+		.when()
+		.body(student)
+		.put("/" + studentId)
+		.then()
+		.log()
+		.all();
+		
+		HashMap<String, Object> value=	SerenityRest.rest()
+				.given()
+				.when()
+				.get("/list")
+				.then()
+				.log()
+				.all()
+				.statusCode(200)
+				.extract()
+				.path("findAll{it.firstName=='"+firstName+"'}.get(0)");
+			
+			System.out.println("The value is " + value);
+			studentId = (int)value.get("id");
+			
+			assertThat(value, hasValue(firstName));
+		
+	}
+	
+	@Title("Delete the Student and veify if student is deleted")
+	@Test
+	public void test004(){
+		SerenityRest.rest().given()
+		.when()
+		.delete("/" + studentId)
+		.then()
+		.log()
+		.all();
+		
+		
+		SerenityRest.rest()
+		.given()
+		.when()
+		.get("/"+studentId)
+		.then()
+		.log()
+		.all()
+		.statusCode(404);
+	
+	}
 }
