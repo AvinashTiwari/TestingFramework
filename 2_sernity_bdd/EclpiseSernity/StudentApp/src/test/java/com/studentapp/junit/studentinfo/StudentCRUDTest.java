ab2593b362d473e856f350a4edf35ff84cad6ddc
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-12 00:44:55 -0600

    no message

--- 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/studentinfo/StudentCRUDTest.java
+++ 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/junit/studentinfo/StudentCRUDTest.java
@@ -22,119 +22,66 @@ import net.thucydides.core.annotations.Title;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 
-
 @RunWith(SerenityRunner.class)
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class StudentCRUDTest extends TestBase {
-	
-	static String firstName =TestUtil.getRandomValue()+ " -Avinash";
-	static String lastName =TestUtil.getRandomValue()+ "  -Tiwari";
+
+	static String firstName = TestUtil.getRandomValue() + " -Avinash";
+	static String lastName = TestUtil.getRandomValue() + "  -Tiwari";
 	static String programme = "ComputerScience";
-	static String email =  TestUtil.getRandomValue()+"abc@12376778.com";
-	static int studentId ;
-	
+	static String email = TestUtil.getRandomValue() + "abc@12376778.com";
+	static int studentId;
+
 	@Steps
 	StudentSerenitySteps studentSerenitySteps;
-	
+
 	@Title("This test Will Create New Student")
 	@Test
-	public void test001(){
-		
+	public void test001() {
 
 		ArrayList<String> courses = new ArrayList<String>();
 		courses.add("Java");
 		courses.add("C++");
-		
-		studentSerenitySteps.createStudent(firstName, lastName, email, programme, courses)
-		.statusCode(201);
-		
+
+		studentSerenitySteps.createStudent(firstName, lastName, email, programme, courses).statusCode(201);
+
 	}
-	
-	
+
 	@Title("Student was added to the  applciation")
 	@Test
-	public void test002(){
-	HashMap<String, Object> value=	SerenityRest.rest()
-		.given()
-		.when()
-		.get("/list")
-		.then()
-		.log()
-		.all()
-		.statusCode(200)
-		.extract()
-		.path("findAll{it.firstName=='"+firstName+"'}.get(0)");
-	
-	System.out.println("The value is " + value);
-	studentId = (int)value.get("id");
-	
-	assertThat(value, hasValue(firstName));
+	public void test002() {
+		HashMap<String, Object> value = studentSerenitySteps.getStudentInfoByFirstName(firstName);
+		System.out.println("The value is " + value);
+		studentId = (int) value.get("id");
+
+		assertThat(value, hasValue(firstName));
 	}
 
 	@Title("Update the User Information and Verify updated information")
 	@Test
-	public void test003(){
-		
-		ArrayList<String> course = new ArrayList<String>();
-		course.add("Java");
-		course.add("C++");
+	public void test003() {
+
+		ArrayList<String> courses = new ArrayList<String>();
+		courses.add("Java");
+		courses.add("C++");
 		firstName = firstName + "_updated";
-		
-		StudentClass student = new StudentClass();
-		student.setFirstName(firstName);
-		student.setLastName(lastName);
-		student.setProgramme(programme);
-		student.setEmail(email);
-		student.setCourses(course);
-		
-		SerenityRest.rest().given()
-		.contentType(ContentType.JSON)
-		.log()
-		.all()
-		.when()
-		.body(student)
-		.put("/" + studentId)
-		.then()
-		.log()
-		.all();
-		
-		HashMap<String, Object> value=	SerenityRest.rest()
-				.given()
-				.when()
-				.get("/list")
-				.then()
-				.log()
-				.all()
-				.statusCode(200)
-				.extract()
-				.path("findAll{it.firstName=='"+firstName+"'}.get(0)");
-			
-			System.out.println("The value is " + value);
-			studentId = (int)value.get("id");
-			
-			assertThat(value, hasValue(firstName));
-		
+		studentSerenitySteps.updateStudent(firstName, lastName, email, programme, courses,studentId);
+
+		HashMap<String, Object> value = studentSerenitySteps.getStudentInfoByFirstName(firstName);
+
+		System.out.println("The value is " + value);
+		studentId = (int) value.get("id");
+
+		assertThat(value, hasValue(firstName));
+
 	}
-	
+
 	@Title("Delete the Student and veify if student is deleted")
 	@Test
-	public void test004(){
-		SerenityRest.rest().given()
-		.when()
-		.delete("/" + studentId)
-		.then()
-		.log()
-		.all();
+	public void test004() {
 		
-		
-		SerenityRest.rest()
-		.given()
-		.when()
-		.get("/"+studentId)
-		.then()
-		.log()
-		.all()
-		.statusCode(404);
-	
+		studentSerenitySteps.deleteStudent(studentId);
+		studentSerenitySteps.getStudent(studentId).statusCode(404);
+
 	}
 }
