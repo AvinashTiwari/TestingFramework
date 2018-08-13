0a1098fe3ee4ee98b8ee81217baa0ac63372291a
Avinash Tiwari <qwe123kids@gmail.com>
2018-08-12 22:32:54 -0600

    no message

--- 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/cucumber/serenity/StudentSerenitySteps.java
+++ 2_sernity_bdd/EclpiseSernity/StudentApp/src/test/java/com/studentapp/cucumber/serenity/StudentSerenitySteps.java
@@ -84,5 +84,15 @@ public class StudentSerenitySteps {
 		return SerenityRest.rest().given().when().get("/"+studentId).then();
 
 	}
+	
+	@Step
+	public HashMap<String, Object> getStudentInfoByEmailId(String email) {
+
+		String p1 = "findAll{it.email=='";
+		String p2 = "'}.get(0)";
+		return SerenityRest
+				.rest().given().when().get("/list").then().extract()
+				.path(p1 + email + p2);
+	}
 
 }
