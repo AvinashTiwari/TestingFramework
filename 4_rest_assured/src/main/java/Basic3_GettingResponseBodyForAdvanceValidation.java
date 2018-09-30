f102a1de39d783301130705ba10c7e23d489cf0c
Avinash Tiwari <qwe123kids@gmail.com>
2018-09-29 23:23:22 -0600

    no message

--- 4_rest_assured/src/main/java/Basic3_GettingResponseBodyForAdvanceValidation.java
+++ 4_rest_assured/src/main/java/Basic3_GettingResponseBodyForAdvanceValidation.java
@@ -2,16 +2,30 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class Basic3_GettingResponseBodyForAdvanceValidation {
+    Properties prop = new Properties();
+
+    @BeforeTest()
+    public void getData() throws IOException {
+        FileInputStream fis = new FileInputStream(".\\src\\files\\env.properties");
+        prop.load(fis);
+    }
 
     @Test
     public void AddAndDeletePlace(){
-        RestAssured.baseURI="http://216.10.245.166";
+        RestAssured.baseURI=        prop.getProperty("HOST");
+        System.out.println("HOST Value " + prop.getProperty("HOST"));
        Response res = given()
                 .queryParam("keyword", "cruise")
                 .body("{\n" +
@@ -44,7 +58,7 @@ public class Basic3_GettingResponseBodyForAdvanceValidation {
 
      JsonPath jsonPath = new JsonPath(resString);
       String palceId =   jsonPath.get("place_id");
-        System.out.println(palceId);
+        System.out.println("This Place Id " + palceId);
 
         /*
         * {"status":"OK","place_id":"8b051e1218d121cd9d4530ebbf704cd8","scope":"APP","reference":"4a08226c965c238f6bad7849ecd767a14a08226c965c238f6bad7849ecd767a1","id":"4a08226c965c238f6bad7849ecd767a1"}
@@ -52,9 +66,16 @@ public class Basic3_GettingResponseBodyForAdvanceValidation {
 
         * */
 
+String todelete ="{";
+        todelete = todelete + "\"place_id\":\""+palceId+"\"";
+        todelete = todelete + "}";
+        System.out.println(" To delete String " +  todelete);
+
+
+
         given()
                 .queryParam("key", "qaclick123")
-                .body("{\"place_id\":\"+palceId+\"}")
+                .body( todelete)
                 .when()
                 .post("maps/api/place/delete/json")
                 .then()
