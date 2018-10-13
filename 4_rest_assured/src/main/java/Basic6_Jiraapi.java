00b14abc6861b2b3cbfe4ecbe6d6bdf558b23532
Avinash Tiwari <qwe123kids@gmail.com>
2018-10-12 18:21:58 -0600

    no message

--- 4_rest_assured/src/main/java/Basic6_Jiraapi.java
+++ 4_rest_assured/src/main/java/Basic6_Jiraapi.java
@@ -1,2 +1,39 @@
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Basic6_Jiraapi {
+    Properties prop = new Properties();
+
+    @BeforeTest()
+    public void getData() throws IOException {
+        FileInputStream fis = new FileInputStream(".\\src\\files\\env.properties");
+        prop.load(fis);
+    }
+
+    @Test
+    public void JiraApi_Post(){
+        RestAssured.baseURI=        "http://localhost:8080";
+        Response res = given().header("Content-Type", "application/json")
+                .header("Cookie", "JSESSIONID="+ReuseableMethod.getSessionKey())
+                .body("{\"fields\" : { \"project\" : {  \"key\" : \"RES\" }, \"summary\" : \"For comment Jira Posy Resr Test\", \"description\" :  \"Creating  first Bug By test class\" , \"issuetype\" : { \"name\" : \"Bug\" }}}")
+                .when()
+                .post("/rest/api/2/issue")
+                .then()
+                .statusCode(201)
+                .extract()
+                .response();
+        JsonPath js =  ReuseableMethod.rawToJson(res);
+        String session =  js.get("id");
+        System.out.println(session);
+
+
+    }
+
 }
--- 4_rest_assured/src/main/java/ReuseableMethod.java
+++ 4_rest_assured/src/main/java/ReuseableMethod.java
@@ -1,6 +1,9 @@
+import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.path.xml.XmlPath;
 import io.restassured.response.Response;
+
+import static io.restassured.RestAssured.given;
 
 public class ReuseableMethod {
 
@@ -19,5 +22,22 @@ public class ReuseableMethod {
         String respon=r.asString();
         JsonPath x=new JsonPath(respon);
         return x;
+    }
+
+    public  static String getSessionKey(){
+        RestAssured.baseURI=        "http://localhost:8080";
+        Response res = given().header("Content-Type", "application/json")
+                .body("{ \"username\" : \"avit4143\", \"password\" : \"9967541245\" }")
+                .when()
+                .post("/rest/auth/1/session")
+                .then()
+                .statusCode(200)
+                .extract()
+                .response();
+        JsonPath js =  ReuseableMethod.rawToJson(res);
+        String session =  js.get("session.value");
+
+        return session;
+
     }
 }
