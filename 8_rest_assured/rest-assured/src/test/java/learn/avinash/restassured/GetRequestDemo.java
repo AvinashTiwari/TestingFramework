f2c497f35f44da5162b01c09a85f66525c6286e1
AvinashTiwari85 <qwe123kids@gmail.com>
2019-08-24 15:00:58 -0600

    no message

---
+++ 8_rest_assured/rest-assured/src/test/java/learn/avinash/restassured/GetRequestDemo.java
@@ -0,0 +1,50 @@
+package learn.avinash.restassured;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+
+public class GetRequestDemo {
+
+
+    @BeforeClass
+    public void steup(){
+        RestAssured.baseURI = "https://maps.googleapis.com";
+        RestAssured.basePath = "/maps/api";
+
+    }
+
+    @Test
+    public void statusCodeVerification(){
+        given()
+                .param("units","imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations","New+York+City,NY")
+                .param("key", "AIzaSyApBx5kGuTcPcdrCn5cl6lUlrKgLi")
+                .when()
+                        .get("/distancematrix/json")
+                        .then()
+                        .statusCode(200);
+
+    }
+
+    @Test
+    public void getResponseBody(){
+        Response res =
+        given()
+                .param("units","imperial")
+                .param("origins", "Washington,DC")
+                .param("destinations","New+York+City,NY")
+                .param("key", "AIzaSyApBx5kGuTcPcdrCn5cl6lUlrKgLi")
+                .when()
+                .get("/distancematrix/json");
+
+        System.out.println(res.body().asString());
+
+
+    }
+}
