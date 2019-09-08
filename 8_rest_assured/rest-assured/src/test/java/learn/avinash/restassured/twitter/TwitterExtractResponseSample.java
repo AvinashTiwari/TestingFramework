8c5ceeed6fe1777c007754a432e74b523cf0d2b0
AvinashTiwari85 <qwe123kids@gmail.com>
2019-09-08 16:28:40 -0600

    no message

---
+++ 8_rest_assured/rest-assured/src/test/java/learn/avinash/restassured/twitter/TwitterExtractResponseSample.java
@@ -0,0 +1,49 @@
+package learn.avinash.restassured.twitter;
+
+import static io.restassured.RestAssured.given;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class TwitterExtractResponseSample {
+    String consumerKey =  "";
+    String consumerSecret = "";
+    String accessToken = "";
+    String accessSecret = "";
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://api.twitter.com";
+        RestAssured.basePath = "/1.1/statuses";
+        consumerKey =  System.getenv("TWITTER_CONSUMER_API_KEY");
+        consumerSecret = System.getenv("TWITTER_CONSUMER_API_SECRET_KEY");
+        accessToken = System.getenv("TWITTER_ACCESS_TOKEN");
+        accessSecret = System.getenv("TWITTER_ACCESS_TOKEN_SECRET");
+    }
+    @Test
+    public void extractResponse() {
+        Response response =
+                given()
+                        .auth()
+                        .oauth(consumerKey, consumerSecret, accessToken, accessSecret)
+                        .queryParam("status", "My First Tweet")
+                        .when()
+                        .post("/update.json")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        String id = response.path("id_str");
+        System.out.println("The response.path: " + id);
+
+        String responseString = response.asString();
+        System.out.println(responseString);
+
+        JsonPath jsPath = new JsonPath(responseString);
+        String name = jsPath.get("user.name");
+        System.out.println("The username is: " + name);
+    }
+}
---
+++ 8_rest_assured/rest-assured/src/test/java/learn/avinash/restassured/twitter/TwitterGETRequest.java
@@ -0,0 +1,61 @@
+package learn.avinash.restassured.twitter;
+
+import static io.restassured.RestAssured.given;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
+public class TwitterGETRequest {
+    String consumerKey =  "";
+    String consumerSecret = "";
+    String accessToken = "";
+    String accessSecret = "";
+    String tweetId = "";
+
+    @BeforeClass
+    public void setup() {
+        RestAssured.baseURI = "https://api.twitter.com";
+        RestAssured.basePath = "/1.1/statuses";
+        consumerKey =  System.getenv("TWITTER_CONSUMER_API_KEY");
+        consumerSecret = System.getenv("TWITTER_CONSUMER_API_SECRET_KEY");
+        accessToken = System.getenv("TWITTER_ACCESS_TOKEN");
+        accessSecret = System.getenv("TWITTER_ACCESS_TOKEN_SECRET");
+    }
+
+    @Test
+    public void postTweet() {
+        Response response =
+                given()
+                        .auth()
+                        .oauth(consumerKey, consumerSecret, accessToken, accessSecret)
+                        .queryParam("status", "My First Tweet")
+                        .when()
+                        .post("/update.json")
+                        .then()
+                        .statusCode(200)
+                        .extract()
+                        .response();
+
+        tweetId = response.path("id_str");
+        System.out.println("The response.path: " + tweetId);
+    }
+
+    @Test(dependsOnMethods={"postTweet"})
+    public void readTweet() {
+        Response response =
+                given()
+                        .auth()
+                        .oauth(consumerKey, consumerSecret, accessToken, accessSecret)
+                        .queryParam("id", tweetId)
+                        .when()
+                        .get("/show.json")
+                        .then()
+                        .extract()
+                        .response();
+
+        String text = response.path("text");
+        System.out.println("The tweet text is: " + text);
+    }
+}
