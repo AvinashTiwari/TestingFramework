85f45987b9e97d9757da95ed637dd027a0735000
AvinashTiwari85 <qwe123kids@gmail.com>
2019-10-06 11:55:20 -0600

    no message

---
+++ 9_rest_assured/Chap1/src/test/java/allurereport/CalculatorSteps.java
@@ -0,0 +1,28 @@
+package allurereport;
+
+
+import io.qameta.allure.Attachment;
+import io.qameta.allure.Step;
+import learn.avinash.restassured.allure.MySimpleCalculator;
+
+public class CalculatorSteps {
+	
+	
+	@Step("Adding 2 numbers: {0} & {1}")
+	public double add2Numbers(double n1, double n2) {
+		
+		MySimpleCalculator calculate = new MySimpleCalculator();
+		return calculate.add2Num(n1, n2);
+	}
+	
+	@Attachment(value="{0}" , type = "text/plain")
+	public String writeText( String message) {
+		return message;
+	}
+	
+	@Attachment(value="Image" , type = "image/png")
+	public byte[] attachImage( byte[] image) {
+		return image;
+	}
+
+}
