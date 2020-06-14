9f8a75ca53f8d4f2fd88b63b1d88cf824d9258fe
AvinashTiwari85 <qwe123kids@gmail.com>
2020-06-13 18:31:32 -0700

    no message

--- 15_Powershell/1_InstallHyperV.ps1
+++ 15_Powershell/1_InstallHyperV.ps1
@@ -1,2 +1,13 @@
 ﻿Get-windowsFeature -Name Hyper-V |
   Select-Object -Property Name, Installed
+
+Add-WindowsFeature -Name Hyper-V -IncludeAllSubFeature  -IncludeManagementTools -Restart
+
+$Splat = @{
+Name ="Hyper-V"
+IncludeAllSubFeature = $True
+IncludeManagementTool = $True
+Restart = $True
+}
+
+Add-WindowsFeature @Splat
