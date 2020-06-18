eb9858487a1d1eaac1bcdf18dd794e537f69e88a
AvinashTiwari85 <qwe123kids@gmail.com>
2020-06-18 15:44:58 -0600

    no message

---
+++ 15_Powershell/2_Scope1.ps1
@@ -0,0 +1,7 @@
+﻿Write-Output "The Value of X is : $X"
+Write-Output "Incrementing X by 10"
+
+$X +=10
+Write-Output "The Value of X is : $X"
+Write-Output "============================="
+Write-Output " "
