58aa0c44dd29421b0e01a3a68985b8e2e7582a78
AvinashTiwari85 <qwe123kids@gmail.com>
2020-06-24 15:22:02 -0600

    no message

---
+++ 15_Powershell/2_Valiation_1.ps1
@@ -0,0 +1,19 @@
+﻿Function Get-CounterData
+{
+Param(
+      [Parameter(Mandatory=$true)]
+      [ValidateSet("Processor", "Disk", "Memory")]
+      [string]
+      $Data, 
+      [ValidateRange(1,10)]
+      [int]$MaxSample = 1,
+      [ValidateRange(1,10)]
+      [int]$SampleInterval = 1)
+    Switch($Data) {
+     "Processor" {$Counter = "\Processor(_total)\% Processor Time"}
+     "Disk" {$Counter = "\PhysicalDisk(0*)\% Disk Time"}
+     "Memory" {$Counter = "\Memory\Page Fault/sec"}
+    }
+
+    Get-Counter -Counter $Counter -MaxSamples  $MaxSample  -SampleInterval  $SampleInterval
+}
