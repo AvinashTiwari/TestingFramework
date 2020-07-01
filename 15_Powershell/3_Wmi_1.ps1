b2c6378bdd4d696e2496ad56310401cdae4ca726
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-01 13:24:51 -0600

    no message

---
+++ 15_Powershell/3_Wmi_1.ps1
@@ -0,0 +1,14 @@
+﻿Get-WMIObject -Namespace "root" -Class "__Namespace" | select Name
+
+Get-WmiObject -List
+
+Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName Svr1
+
+Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName Svr1 | 
+                Get-Member
+
+(Get-CimClass -ClassName Win32_OperatingSystem -ComputerName Svr1).CimClassMethods
+
+Get-CimInstance -ClassName Win32_OperatingSystem -ComputerName Svr1 |
+               Invoke-CimMethod -MethodName Reboot
+
