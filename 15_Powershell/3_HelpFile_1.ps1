4523f264209c090ec8f35dd5d121ede46b687f4b
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-03 13:22:30 -0600

    no message

---
+++ 15_Powershell/3_HelpFile_1.ps1
@@ -0,0 +1,51 @@
+﻿# Help File - Code 1
+
+Function DO-CoolThings
+{
+<#
+.SYNOPSIS
+Does something cool
+
+.DESCRIPTION
+Does Something really cool.
+
+.PARAMETER Name
+The name of the cool person.
+
+.PARAMETER Age
+The age of the cool person.
+
+.PARAMETER Phone
+The phone number of the cool person.
+
+.EXAMPLE
+DO-CoolThings -Name "Jason" -Age 25 -Phone 123-456-7890
+
+Makes this person really cool!
+
+.EXAMPLE
+DO-CoolThings -Name "Jason" -Age 30 -Phone 123-456-7890 | Select-Object -Property Phone
+
+Displays the cool persons number.
+
+
+#>
+Param (
+
+    [parameter(Mandatory=$true)]
+    [String[]]
+    $Name,
+    
+    [parameter(Mandatory=$true,
+    ValueFromPipelineByPropertyName=$true)]
+    [int]
+    $Age,
+
+    
+    [ValidatePattern("[0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]")]
+    [String]
+    $Phone
+)
+
+}
+Get-Help DO-CoolThings -full
