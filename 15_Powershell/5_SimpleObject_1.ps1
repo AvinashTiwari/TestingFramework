f2b47f4100204cb0886abde2d15ebcb7740e5d34
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-05 13:06:23 -0600

    no message

---
+++ 15_Powershell/5_SimpleObject_1.ps1
@@ -0,0 +1,32 @@
+﻿Get-Process |
+   Select-Object -First 5 -Property Name, CPU, VM, WS
+
+
+
+Get-Process |
+    Select-Object -First 5 -Property Name, CPU ,
+      @{
+      
+         Name="VMWS"
+         Expression = {$_.VM + $_.WS}
+      }
+
+
+$Obj = New-Object -TypeName PSObject -Property @{
+
+    Name = $null
+    Date = Get-Date -Format ddmmyy
+    City = $null
+}
+
+$obj.Name = "Avinash"
+$obj.City =  "denver"
+
+$obj
+
+$obj |Get-Member
+
+$obj.PSObject.TypeNames.Insert(0, 'MyObject.UserInfo')
+$obj | Get-Member
+
+
