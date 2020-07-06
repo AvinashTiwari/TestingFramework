aba700cc8ac9abb8d13f2e67533f9e7b6657fccf
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-06 14:40:05 -0600

    no message

---
+++ 15_Powershell/5_MultipleSource_2.ps1
@@ -0,0 +1,17 @@
+﻿$Names = "DC1", "Svr1"
+
+forEach($N in $Names)
+{
+   $OS = Get-CimInstance -ComputerName $N -ClassName Win32_OperatingSystem
+   $Bios = Get-CimInstance -ComputerName $N -ClassName Win32_Bios
+
+   $Obj = New-Object -TypeName PSObject -Property @{
+      Caption = $OS.Caption
+      OSVersion = $OS.Version
+      BiosSerial = $Bios.SerialNumber
+   }
+
+   Write-Output $Obj |
+             Where-Object Caption -Like "Microsoft*"
+
+}
---
+++ 15_Powershell/5_MultipleSource_4.ps1
@@ -0,0 +1,21 @@
+﻿$Names = "DC1", "Svr1"
+
+$Data = forEach($N in $Names)
+{
+   $OS = Get-CimInstance -ComputerName $N -ClassName Win32_OperatingSystem
+   $Bios = Get-CimInstance -ComputerName $N -ClassName Win32_Bios
+
+   $Obj = New-Object -TypeName PSObject -Property @{
+      Caption = $OS.Caption
+      OSVersion = $OS.Version
+      BiosSerial = $Bios.SerialNumber
+   }
+
+   Write-Output $Obj 
+
+}
+$Data | Select-Object -Property Caption, @{
+
+          Name = "Version"
+          Expression = {$_.OSversion.Remove(3)}
+}
