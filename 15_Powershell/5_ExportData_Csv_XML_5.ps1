6e08fe082936fb0417cca05288b0c8d4251d7f45
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-07 16:20:43 -0600

    no message

---
+++ 15_Powershell/5_ExportData_Csv_XML_5.ps1
@@ -0,0 +1,12 @@
+﻿Get-AdUser -Filter {Name -eq 'Administrator'} -Properties MemberOf
+
+Get-ADUser -Filter {Name -eq 'Administrator'} -Properties MemberOf |
+            Export-Csv C:\PowerShell\Admin.csv
+
+Import-Csv -Path C:\PowerShell\Admin.csv
+
+
+Get-ADUser -Filter {Name -eq 'Administrator'} -Properties MemberOf |
+     Export-Clixml -Path C:\PowerShell\Admin.Xml
+
+Import-Clixml -Path C:\PowerShell\Admin.Xml
