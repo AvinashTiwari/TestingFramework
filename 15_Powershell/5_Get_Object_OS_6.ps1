7f7e87c7e4d11569c99468991a7d33179d146b63
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-08 13:44:27 -0600

    no message

---
+++ 15_Powershell/5_Get_Object_OS_6.ps1
@@ -0,0 +1,13 @@
+﻿$OS = Get-CimInstance -Class Win32_OperatingSystem
+
+$OS | Get-Member
+
+$OS.InstallDate | Get-Member
+
+$OS.InstallDate | Select-object -ExpandProperty DayOfYear
+
+$OS.InstallDate.DayOfYear
+
+$User =  Get-ADUser -Filter {Name -eq "Administrator" } -Properties MemberOf
+$User
+$User.MemberOf
