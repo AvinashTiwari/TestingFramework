2d46e12d370e3cdb07a5b0a967352b7f52d08adf
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-09 12:00:40 -0600

    no message

---
+++ 15_Powershell/5_Method_Code_7.ps1
@@ -0,0 +1,28 @@
+﻿$env:PSModulePath
+$env:PSModulePath | Get-Member -MemberType Method
+($env:PSModulePath).Split(";")
+
+Get-Date | Get-Member
+
+(Get-Date).DayOfWeek
+
+(Get-Date).AddDays(5)
+(Get-Date).AddDays(-10)
+
+$Date = (Get-Date).AddDays(-2)
+Get-EventLog -LogName System -After $Date
+
+$obj = New-Object -TypeName PSObject -Property @{
+   CurrentSpeed = 0
+}
+
+$Obj | Format-Table -AutoSize
+
+$Obj | Get-Member
+
+$Obj | Add-Member -MemberType ScriptMethod
+                  -Name "Accelerate"
+                  -Value {This.CurrentSpeed += 5}
+$Obj | GM
+
+$Obj.Accelerate()
