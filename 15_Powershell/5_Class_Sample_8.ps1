22cc255444ae4e3bca8db99585e32806e1d5d846
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-10 14:07:52 -0600

    no message

---
+++ 15_Powershell/5_Class_Sample_8.ps1
@@ -0,0 +1,8 @@
+﻿ class Vehicle {
+
+[String] $Directon = "North"
+[Int] $Speed = 0
+
+}
+
+[Vehicle]::New()
---
+++ 15_Powershell/5_Enum_Sample_9.ps1
@@ -0,0 +1,22 @@
+﻿Enum Course {
+   North
+   NorthEast
+   East
+   SouthEast
+   South
+   SouthWest
+   West
+   NorthWest
+  
+}
+
+[Enum]::GetName([Course])
+
+Class Vehicle{
+  [Course] $Direction = 2
+  [Int] $Speed = 0
+
+}
+
+[Vehicle]::New()
+[Vehicle]::New() | GM
