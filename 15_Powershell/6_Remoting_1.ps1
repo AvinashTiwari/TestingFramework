ae6fd9c55993ce4eb2855ae0654b1d430fbd92d1
AvinashTiwari85 <qwe123kids@gmail.com>
2020-07-15 14:39:10 -0600

    no message

---
+++ 15_Powershell/6_Remoting_1.ps1
@@ -0,0 +1,10 @@
+﻿Get-PSSessionConfiguration
+
+Get-Command -ParameterName ComputerName
+
+Get-Command -ParameterName Session
+
+Get-Command -ParameterName CIMSession
+
+Get-Help about_*remote* | Select-Object -Property Name, Synopsis
+Get-Help about_*session* | Select-Object -Property Name, Synopsis
