3b9029dd90095d4c2a2da36afe4fb0943a387a41
AvinashTiwari85 <qwe123kids@gmail.com>
2021-01-01 16:55:06 -0700

    no message

---
+++ Generic/src/main/java/Generic/wildcard/Main.java
@@ -0,0 +1,18 @@
+package Generic.wildcard;
+
+public class Main {
+    public static void main(String[] args) {
+        var countries = new String[] {"USA", "INDIA", "UK"};
+        var scores = new Integer[]{22,64,55};
+        var names = new String[] {"A", "B"};
+        var store1 =  new Store<>(countries);
+        var store2 =  new Store<>(scores);
+        var store3 = new Store<>(names);
+
+        System.out.println(store1.list.length);
+        System.out.println(store2.list.length);
+
+        store1.greaterThen(store2);
+
+    }
+}
--- Generic/src/main/java/Generic/wildcard/Store.java
+++ Generic/src/main/java/Generic/wildcard/Store.java
@@ -1,5 +1,18 @@
 package Generic.wildcard;
 
 public class Store <T> {
-  T value;
+  T[] list;
+
+  public Store(T[] list){
+   this.list = list;
+  }
+
+  public void greaterThen(Store<?> otherStore){
+    if(list.length > otherStore.list.length){
+      System.out.println("TRUE");
+    }else
+    {
+      System.out.println("FALSE");
+    }
+  }
 }
