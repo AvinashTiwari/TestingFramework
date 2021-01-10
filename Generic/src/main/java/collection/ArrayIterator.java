2ad4f4b7cb992eeae53e9ac42df17f2448755a80
AvinashTiwari85 <qwe123kids@gmail.com>
2021-01-10 13:06:51 -0700

    no message

---
+++ Generic/src/main/java/collection/ArrayIterator.java
@@ -0,0 +1,21 @@
+package collection;
+
+import java.util.Arrays;
+
+public class ArrayIterator {
+    public static void main(String[] args) {
+        var list = Arrays.asList(3,4,5,6,7);
+        var iterate = list.iterator();
+
+        while(iterate.hasNext()){
+            System.out.println(iterate.next());
+        }
+
+
+        var iterates = list.listIterator();
+        while(iterates.hasNext()){
+            System.out.println("list " + iterates.next());
+        }
+
+    }
+}
---
+++ Generic/src/main/java/collection/ArrayUsing.java
@@ -0,0 +1,12 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class ArrayUsing {
+    public static void main(String[] args) {
+        var list = new ArrayList<>(Arrays.asList(3,4,5,6,7));
+        list.add(90);
+        System.out.println(list.get(1));
+    }
+}
