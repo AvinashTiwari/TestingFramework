9837e34ff63436607129d03929ca8209811c4b01
AvinashTiwari85 <qwe123kids@gmail.com>
2021-01-16 17:06:51 -0700

    no message

--- Zookeeper/src/main/java/tfidf/search/TFIDF.java
+++ Zookeeper/src/main/java/tfidf/search/TFIDF.java
@@ -1,6 +1,9 @@
 package tfidf.search;
 
+import tfidf.model.DocumentData;
+
 import java.util.List;
+import java.util.Map;
 
 public class TFIDF {
     public  static double calculateTermFrequency(List<String> words, String term){
@@ -12,5 +15,28 @@ public class TFIDF {
        }
        double termfrequency = (double) count / words.size();
        return  termfrequency;
+    }
+
+    public static DocumentData createDocument(List<String> words, List<String> terms){
+        DocumentData documentData = new DocumentData();
+        for(String term: terms){
+            double termFreq = calculateTermFrequency(words, term);
+            documentData.putTermFrequency(term, termFreq);
+        }
+       return  documentData;
+    }
+
+    private static double getInverseDocumentFrequency(String term, Map<String, DocumentData > documentResults){
+      double nt =0;
+      for(String document: documentResults.keySet()){
+          DocumentData documentData =  documentResults.get(document);
+          double termFrequency = documentData.getFrequency(term);
+          if(termFrequency > 0.0){
+              nt++;
+          }
+
+      }
+
+      return  nt == 0 ? 0: Math.log10(documentResults.size() /nt);
     }
 }
