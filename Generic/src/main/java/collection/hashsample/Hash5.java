736ced4bebeb382ba0e0303de5f3b00cbc6672d7
AvinashTiwari85 <qwe123kids@gmail.com>
2021-01-22 16:23:02 -0700

    no message

---
+++ Generic/src/main/java/collection/hashsample/Hash5.java
@@ -0,0 +1,18 @@
+package collection.hashsample;
+
+import java.util.LinkedList;
+
+public class Hash5 {
+    public static void main(String[] args) {
+        var list = new LinkedList<>();
+        list.add(5);
+        list.add(6);
+
+        list.addFirst(7);
+        list.addLast(8);
+
+        var firstppek = list.peek();
+
+
+    }
+}
